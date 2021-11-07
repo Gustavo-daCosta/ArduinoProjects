b332eaf9cc9cf1e864d68d6b7bf02c3e2267c7a0
Gustavo Gonçalves da Costa <guti.ps.hzd@gmail.com>
2021-11-07 19:02:45 -0300

    commit

---
+++ Projetos/Automated_Dino_Game/Automated_Dino_Game.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+#define limiar 260     // 250, 260, 300
+#define unpress_angle 90 
+#define press_angle 45     
+
+Servo myservo;
+bool trig=true;
+
+void setup() {    
+  myservo.attach(9);
+  myservo.write(unpress_angle);   
+}
+
+void loop() {
+ myservo.write(unpress_angle);              // Parar de apertar a barra de espaço
+ delay(1);
+ if(analogRead(A0)< limiar)
+ {
+    myservo.write(press_angle);          // Apertar a barra de espaço
+    delay(100);                   
+ }
+}
---
+++ Testes/Teste_tela_LCD/Teste_tela_LCD.ino
@@ -0,0 +1,36 @@
+#include <LiquidCrystal.h>                                                                                                                                                  
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ 
+void setup()
+{
+  //Define o número de colunas e linhas do LCD
+  lcd.begin(16, 2);
+}
+ 
+void loop()
+{
+  //Limpa a tela
+  lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(3, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("FILIPEFLOP");
+  lcd.setCursor(3, 1);
+  lcd.print(" LCD 16x2");
+  delay(5000);
+   
+  //Rolagem para a esquerda
+  for (int posicao = 0; posicao < 3; posicao++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(300);
+  }
+   
+  //Rolagem para a direita
+  for (int posicao = 0; posicao < 6; posicao++)
+  {
+    lcd.scrollDisplayRight();
+    delay(300);
+  }
+}
